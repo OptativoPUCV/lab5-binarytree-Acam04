0316c5467fd0a7f0e720c1874e84c2bbcef4d7aa
Your Name <you@example.com>
2024-05-08 15:13:29 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -37,14 +37,41 @@ TreeNode * createTreeNode(void* key, void * value) {
 }
 
 TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
-
-    //new->lower_than = lower_than;
-    return NULL;
+    TreeMap * new = (TreeMap *)malloc(sizeof(TreeMap));
+    new->lower_than = lower_than;
+    new->root = NULL;
+    new->current = NULL;
+    return new;
 }
 
 
 void insertTreeMap(TreeMap * tree, void* key, void * value) {
-
+    if (tree == NULL || tree->root == NULL) return;
+    if (searchTreeMap(tree, key) != NULL) return;
+    TreeNode * parent = NULL;
+    TreeNode * current = tree->root;
+    while (current != NULL)
+    {
+        if (tree->lower_than(key, current->pair->key)){
+            parent = current; 
+            current = current->left;
+        }
+        else{
+            parent = current;
+            current = current->right;
+        }
+    }
+    TreeNode * new = createTreeNode(key, value);
+    if (parent == NULL) tree->root = new;
+    else{
+        if (tree->lower_than(key, parent->pair->key) == 1){
+            parent->left = new;
+        }
+        else{
+            parent->right = new;
+        }
+        new->parent = parent;
+    }
 }
 
 TreeNode * minimum(TreeNode * x){
